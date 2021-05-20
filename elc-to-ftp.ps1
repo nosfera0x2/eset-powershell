fa360070cf2dbda97b1dd547d07e2165ed309d38
spence-rat <82817752+spence-rat@users.noreply.github.com>
2021-05-20 15:24:11 -0500

    Update elc-to-ftp.ps1
    Added comments

--- elc-to-ftp.ps1
+++ elc-to-ftp.ps1
@@ -1,11 +1,16 @@
 #tested on 0520221 - works
 Set-ExecutionPolicy Bypass -Scope Process
-#prompt for name of company
+#prompt for name of company, later used as name of archive.
+
 $company = read-host 'What is your company name?'
+
 #source of current ELC package
+
 $source = 'https://download.eset.com/com/eset/tools/diagnosis/log_collector/latest/esetlogcollector.exe'
 
 Set-Variable -Name destination -Value ($env:userprofile)
+
+#Tests to see if esetlogcollector.exe is already in the user's downloads folder.
 
 If((Test-Path -Path $destination\Downloads\esetlogcollector.exe -PathType Leaf) -eq 'True') {
 
@@ -16,10 +21,15 @@ else {
 
 	Invoke-WebRequest -uri $source -outfile $destination\downloads\esetlogcollector.exe
 }
+#runs the ESET Log Collector with profile set to all
 
 cmd /c "start /wait %USERPROFILE%\Downloads\esetlogcollector.exe /accepteula /otype:obin /profile:all %USERPROFILE%\Downloads\eset_logs_ps.zip"
 
+#renames the eset_logs_ps file to an archive with name of the company
+
 Rename-item $env:userprofile\downloads\eset_logs_ps.zip -NewName $env:userprofile\downloads\"$company.zip"
+
+#this defines a function which allows for anonymous uploads to the ESET FTP server. 
 
 function SendByFTP {
     param (
